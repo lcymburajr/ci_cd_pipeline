432de0cf628a71327678c987459993568c086ba1
Lawrence Cymbura <lcymbura@key2act.com>
2021-02-07 17:40:03 -0500

    update kube

--- scripts/updateKube.sh
+++ scripts/updateKube.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" k8s/api-deployment.yml
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}" ../k8s/api-deployment.yml
 cat k8s/api-deployment.yml
 
-sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}/g" k8s/client-deployment.yml
+sed -i "s/VERSION/${TRAVIS_BUILD_NUMBER}" ../k8s/client-deployment.yml
 cat k8s/client-deployment.yml
